c4b17a10343948ec73fe8157aa166007080558c8
zqiiuz <zqiiuz@163.com>
2022-05-07 10:27:29 +0800

    first

---
+++ src/main/java/testgit.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: zqzhen
+ * @Date: 2022/05/07/9:25
+ * @Description:
+ */
+public class testgit {
+    public static void main(String[] args) {
+        System.out.println("test git");
+    }
+}
